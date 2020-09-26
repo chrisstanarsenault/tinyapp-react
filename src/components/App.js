1929693f41cfaf68bebc6e85fec7d08784257c87
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-09-26 01:05:49 -0400

    create UrlFullContainer component to house url table, and created basic table layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+
+import UrlFullContainer from "./UrlFullContainer";
 
 import "../styles/App.scss";
 
 function App() {
-  const [urls, setUrls] = useState({});
-  let getApi = async () => {
-    const response = await axios.get("http://localhost:8080/urls");
-    setUrls(response.data.module.urlDatabase);
-  };
-  return <div className="App"></div>;
+  return (
+    <div>
+      <UrlFullContainer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/UrlFullContainer.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function UrlFullContainer() {
+  const [urls, setUrls] = useState({});
+
+  const getApi = async () => {
+    const response = await axios.get("http://localhost:8080/urls");
+    setUrls(response.data.module.urlDatabase);
+  };
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  const urlTableBody = Object.keys(urls).map((keyName, keyIndex) => {
+    return (
+      <tr key={`url-${keyIndex}`}>
+        <td>{keyName}</td>
+        <td>{urls[keyName]}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <main style={{ margin: "1em" }}>
+      <h3>My URLs</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Short URL</th>
+            <th scope="col">Long URL</th>
+          </tr>
+        </thead>
+        <tbody>{urlTableBody}</tbody>
+      </table>
+    </main>
+  );
+}
